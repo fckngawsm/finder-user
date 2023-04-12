d48334a6be4e05f49d5cff5a56653559e8241070
Kirill <forworkskirill@gmail.com>
2023-04-12 18:55:48 +0500

    fixed theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import styles from "./Header.module.scss";
 import { ThemeSwitcher } from "../ThemeSwitcher";
 
 export const Header = () => (
-  <div className={styles.header} data-testid="Header">
+  <div className={styles.header}>
     <h1 className={styles.logo}>finder</h1>
     <ThemeSwitcher />
   </div>
--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -5,15 +5,15 @@ import { ReactComponent as SunIcon } from "../../assets/icon-sun.svg";
 
 export const ThemeSwitcher = () => {
   const [isDark, setIsDark] = useState(false);
-  const themeText = isDark ? "Ligth" : "Dark";
+  const themeText = isDark ? "light" : "dark";
   const ThemeIcon = isDark ? MoonIcon : SunIcon;
 
   useEffect(() => {
-    document.body.setAttribute("data-theme", isDark ? "Dark" : "Light");
+    document.body.setAttribute("data-theme", isDark ? "dark" : "light");
   }, [isDark]);
   return (
     <div className={styles.switcher} onClick={() => setIsDark(!isDark)}>
-      <p>{themeText}</p>
+      <span>{themeText}</span>
       <ThemeIcon className={styles.icon} />
     </div>
   );
